0e0e97798888a3154ef04299d38d3a5b1273cf3d
Adriana <dri19tcc@gmail.com>
2016-06-02 18:25:36 -0700

    Added in flexslider, figured out carousel was not working properly so it was breaking my bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home#boarding'
+  root "home#index"
+  get "/boarding" => "home#boarding", as: "boarding"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
